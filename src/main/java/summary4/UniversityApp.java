32e45d94a2166e3e5f487be63a502e4cbba424f5
StanislavPB <perminovsb@gmail.com>
2023-05-12 13:50:27 +0300

    add summary4

--- src/main/java/summary4/UniversityApp.java
+++ src/main/java/summary4/UniversityApp.java
@@ -10,6 +10,19 @@ import java.util.List;
 public class UniversityApp {
     public static void main(String[] args) {
 
+        /*
+        класс main в котором мы создаем два основных экземпляра классов
+        StudentDBImpl repository = new StudentDBImpl();
+        StudentsService service = new StudentsService(repository);
+
+        в которых будет весь сервисный функционал и работа методов базы данных
+
+        далее мы создаем меню = коллекцию в которой будут экземпляры классов,
+        импл один и тот же интерфейс
+
+        и далее создаем класс меню в котором запускаем метод start
+         */
+
         StudentDBImpl repository = new StudentDBImpl();
         StudentsService service = new StudentsService(repository);
 
