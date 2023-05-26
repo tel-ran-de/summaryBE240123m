567c022c28c8c2cd9370f124059b06aca0052c4a
StanislavPB <perminovsb@gmail.com>
2023-05-26 14:23:57 +0300

    add summary6

--- src/main/java/summary6/smallProject/database/ProductRepositoryImpl.java
+++ src/main/java/summary6/smallProject/database/ProductRepositoryImpl.java
@@ -2,16 +2,19 @@ package summary6.smallProject.database;
 
 import summary6.smallProject.entity.Product;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProductRepositoryImpl implements ProductRepository{
+
+    private List<Product> database = new ArrayList<>();
     @Override
     public void add(Product product) {
-
+        database.add(product);
     }
 
     @Override
     public List<Product> findAll() {
-        return null;
+        return database;
     }
 }
