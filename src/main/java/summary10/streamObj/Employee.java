ac7b71a4226a3331de15891b9f4ebfc10658b8e5
StanislavPB <perminovsb@gmail.com>
2023-06-26 10:28:47 +0300

    add summary 10

---
+++ src/main/java/summary10/streamObj/Employee.java
@@ -0,0 +1,12 @@
+package summary10.streamObj;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+    private String name;
+    private String position;
+    private String department;
+    private Double salary;
+
+}
