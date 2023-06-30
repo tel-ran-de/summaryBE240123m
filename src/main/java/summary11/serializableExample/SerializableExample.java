01f5ac0be465f0e369ab4b93c699b9782dfeb7a8
StanislavPB <perminovsb@gmail.com>
2023-06-30 12:22:53 +0300

    add summary 11

--- src/main/java/summary11/serializableExample/SerializableExample.java
+++ src/main/java/summary11/serializableExample/SerializableExample.java
@@ -1,8 +1,6 @@
 package summary11.serializableExample;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
 
 public class SerializableExample {
     public static void main(String[] args) {
@@ -15,7 +13,24 @@ public class SerializableExample {
             ObjectOutputStream outputStream = new ObjectOutputStream(fileOutputStream);
             outputStream.writeObject(person);
             System.out.println("Наш экземпляр класса Person сохранен в файл");
+            outputStream.close();
+            fileOutputStream.close();
         } catch (IOException e) {}
 
+
+        // Десериализация
+
+        try {
+            FileInputStream fileInputStream = new FileInputStream("person.data");
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+            Person desPerson = (Person) objectInputStream.readObject();
+            System.out.println("Наш экземпляр класса Person восстановлен");
+            System.out.println("Name: " + desPerson.getName());
+            System.out.println("Age: " + desPerson.getAge());
+            objectInputStream.close();
+            fileInputStream.close();
+        } catch (IOException e) {} catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
